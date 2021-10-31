57892a8db8b464acce216f15feea13e417ece54a
unknown <ahmedoueslati122@gmail.com>
2021-10-31 16:22:35 +0100

    menu updated

--- app/src/main/java/tn/esprit/fakerni/Controller/MenuActivity.java
+++ app/src/main/java/tn/esprit/fakerni/Controller/MenuActivity.java
@@ -3,6 +3,9 @@ package tn.esprit.fakerni.Controller;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,12 +14,14 @@ import tn.esprit.fakerni.R;
 
 public class MenuActivity extends AppCompatActivity {
 
-    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_activity);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }

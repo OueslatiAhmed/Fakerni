7a6ec8d877eedda677b2c5a50a1534addc26596c
Scarraa <houcem.chett@gmail.Com>
2021-11-08 16:44:12 +0100

    onSharedPreferencesChangeListener

--- app/src/main/java/tn/esprit/fakerni/Controller/MenuActivity.java
+++ app/src/main/java/tn/esprit/fakerni/Controller/MenuActivity.java
@@ -1,8 +1,12 @@
 package tn.esprit.fakerni.Controller;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -14,6 +18,12 @@ import tn.esprit.fakerni.R;
 
 public class MenuActivity extends AppCompatActivity {
 
+    private SharedPreferences mPreferences;
+    public static final String sharedPrefFile = "tn.esprit.fakerni";
+    private String theme;
+    private String language;
+    private String fingerprint;
+    SharedPreferences.OnSharedPreferenceChangeListener mListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +33,21 @@ public class MenuActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this,  R.id.fragment);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        theme = mPreferences.getString("theme", "light_mode");
+        language = mPreferences.getString("language", "en");
+        fingerprint = mPreferences.getString("fingerprint", "off");
+
+        switch (theme) {
+            case "light_mode":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+            case "dark_mode":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+        }
     }
 }
+
--- app/src/main/java/tn/esprit/fakerni/Controller/SettingsFragment_2.java
+++ app/src/main/java/tn/esprit/fakerni/Controller/SettingsFragment_2.java
@@ -1,15 +1,38 @@
 package tn.esprit.fakerni.Controller;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 import tn.esprit.fakerni.R;
 
-public class SettingsFragment_2 extends PreferenceFragmentCompat {
+public class SettingsFragment_2 extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        PreferenceManager.getDefaultSharedPreferences(this.getContext()).registerOnSharedPreferenceChangeListener(this::onSharedPreferenceChanged);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if(key.equals("theme")) {
+            String theme = sharedPreferences.getString("theme", "light_mode");
+            switch (theme) {
+                case "light_mode":
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    break;
+                case "dark_mode":
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    break;
+            }
+        }
+
     }
 }

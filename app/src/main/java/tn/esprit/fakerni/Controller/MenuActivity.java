efb8fa5f5441baae6e4f71d90de12e2570e1e375
unknown <ahmedoueslati122@gmail.com>
2021-11-11 16:57:10 +0100

    fingerprint

--- app/src/main/java/tn/esprit/fakerni/Controller/MenuActivity.java
+++ app/src/main/java/tn/esprit/fakerni/Controller/MenuActivity.java
@@ -3,12 +3,19 @@ package tn.esprit.fakerni.Controller;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import androidx.biometric.BiometricPrompt;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -16,21 +23,62 @@ import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Locale;
+import java.util.concurrent.Executor;
 
 import tn.esprit.fakerni.R;
 
 
 public class MenuActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
+
     private SharedPreferences mPreferences;
     public static final String sharedPrefFile = "tn.esprit.fakerni";
 
     SharedPreferences.OnSharedPreferenceChangeListener mListener;
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_activity);
+
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(MenuActivity.this,
+                executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode,
+                                              @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                finish();
+                System.exit(0);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(
+                    @NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(),
+                        R.string.authentication_succeeded, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                finish();
+                System.exit(0);
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle(getString(R.string.finger_title))
+                .setSubtitle(getString(R.string.finger_subtitle))
+                .setNegativeButtonText(getString(R.string.finger_negative))
+                .build();
+
+        biometricPrompt.authenticate(promptInfo);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this,  R.id.fragment);
